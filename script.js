efd8932fe47174ea855f1d1fb44a30afe5854970
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-04 00:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+   const title = document.getElementById("title").value;
+   const author = document.getElementById("author").value;
+   const isbn = document.getElementById("isbn").value;
+
+   document.getElementById("book-form").addEventListener("submit", (event) => {
+    	event.preventDefault();
+	    let newRow = document.createElement('tr')
+    	newRow.innerHTML = `
+			<td>${title}<td/>
+            <td>${author}<td/>
+            <td>${isbn}<td/>
+			`;
+       document.getElementById('book-list').appendChild('newRow')
+            // Clear the input fields
+            document.getElementById("title").value = "";
+            document.getElementById("author").value = "";
+            document.getElementById("isbn").value = "";
+
+	
+})

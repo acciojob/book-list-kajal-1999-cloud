a22ac22b36e2acbf64999b0fffac1b7a0de6eaa9
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-04 23:52:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-   const title = document.getElementById("title").value;
-   const author = document.getElementById("author").value;
-   const isbn = document.getElementById("isbn").value;
 
-   document.getElementById("book-form").addEventListener("submit", (event) => {
-    	event.preventDefault();
-	    let newRow = document.createElement('tr')
+   document.getElementById("book-form").addEventListener("submit", function(event) {
+        	event.preventDefault();
+	    
+	       const title = document.getElementById("title").value;
+		   const author = document.getElementById("author").value;
+		   const isbn = document.getElementById("isbn").value;
+
+	    let newRow = document.createElement("tr");
     	newRow.innerHTML = `
-			<td>${title}<td/>
-            <td>${author}<td/>
-            <td>${isbn}<td/>
+			<td>${title}</td>
+            <td>${author}</td>
+            <td>${isbn}</td>
+			 <td><span class="delete">X</span></td>
 			`;
-       document.getElementById('book-list').appendChild('newRow')
+	        // Append the new row to the table
+           document.getElementById("book-list").appendChild(newRow)
             // Clear the input fields
             document.getElementById("title").value = "";
             document.getElementById("author").value = "";
             document.getElementById("isbn").value = "";
+        });
 
-	
-})
+        // Event delegation for delete buttons
+        document.getElementById("book-list").addEventListener("click", function (e) {
+            if (e.target.className === "delete") {
+                e.target.parentElement.parentElement.remove();
+            }
+        });
